c46f99f8c0d9dee4d3541ee7c2964d86e8ea7af9
Marcelo Oroquieta <oroquieta.m@gmail.com>
2025-01-22 18:00:18 -0300

    style(BookCard): update book card design

--- src/app/books/components/BookCard.tsx
+++ src/app/books/components/BookCard.tsx
@@ -8,9 +8,9 @@ export default function BookCard({ book }: { book: Book }) {
   const altText = `Cover of book ${book.title} by ${book.author_name}`;
 
   return (
-    <article className="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
+    <article className="rounded-xl bg-white px-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
       <Link href={`books/${book.key}`}>
-        <div className="relative flex h-80 w-full justify-center overflow-hidden rounded rounded-r-xl">
+        <div className="-translate-y-4 relative flex h-80 w-full justify-center overflow-hidden rounded-t-xl rounded-b-md shadow-lg">
           {/* TODO: place generic book cover image when cover_i is undefined or fetch image fails */}
           <Image
             src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`}
@@ -20,37 +20,27 @@ export default function BookCard({ book }: { book: Book }) {
             className="w-full h-full object-cover"
           />
           {book.ratings_average && (
-            <div className="absolute bottom-3 right-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
+            <div className="absolute backdrop-blur-sm bottom-3 right-3 inline-flex items-center rounded-lg bg-black/25 p-2 shadow-md">
               <Icon
                 name="star"
                 className="text-yellow-400"
               />
-              <span className="ml-1 text-sm text-slate-700">
+              <span className="ml-1 font-semibold text-sm text-white">
                 {book.ratings_average?.toFixed(1)}
               </span>
             </div>
           )}
         </div>
 
-        <div className="mt-2 px-2">
+        <div className="px-2 -translate-y-2">
           <h2 className="flex items-center gap-1 text-slate-700 font-bold">
-            <Icon
-              name="bookmark"
-              className="w-4 h-4"
-              aria-hidden="true"
-            />
-            <span className="flex-1 truncate  antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-blue-gray-900">
+            <span className="flex-1 truncate antialiased tracking-normal font-sans text-md font-semibold leading-relaxed text-blue-gray-900">
               {book.title_suggest}
             </span>
           </h2>
 
-          <p className="flex items-center gap-1 mt-1 text-sm text-slate-400">
-            <Icon
-              name="outuser"
-              className="w-4 h-4"
-              aria-hidden="true"
-            />
-            {book.author_name.join(" - ")}
+          <p className="flex items-center gap-1 text-xs mb-1 text-slate-400">
+            by {book.author_name.join(" - ")}
           </p>
         </div>
       </Link>

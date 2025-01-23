ba6424d84bdebef2d89a35abc3cdbf7241b602fe
Marcelo Oroquieta <oroquieta.m@gmail.com>
2025-01-23 02:22:35 -0300

    style(Counter): increase button text size

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -14,13 +14,13 @@ export default function Counter() {
       <span className="text-9xl">{counter}</span>
       <div className="flex gap-2">
         <button
-          className="middle none center rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          className="middle none center rounded-lg bg-blue-500 py-3 px-6 font-sans text-md font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           data-ripple-light="true"
           onClick={() => handleCounterUpdate(-1)}>
           -1
         </button>
         <button
-          className="middle none center rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          className="middle none center rounded-lg bg-blue-500 py-3 px-6 font-sans text-md font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           data-ripple-light="true"
           onClick={() => handleCounterUpdate(1)}>
           +1

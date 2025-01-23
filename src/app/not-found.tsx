caf617444fd4f35df02574137dac4cd3d541c783
Marcelo Oroquieta <oroquieta.m@gmail.com>
2025-01-23 02:23:21 -0300

    feat(NotFound): add global not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="h-full w-full flex flex-col justify-center items-center grow font-sans">
+      <h1 className="text-9xl font-extrabold text-[#151d2c]">404</h1>
+      <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">Page Not Found</div>
+      <button className="middle none center rounded-lg bg-blue-500 py-3 px-6 font-sans text-md font-bold text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+        <Link href="/dashboard">Go Dashboard</Link>
+      </button>
+    </div>
+  );
+}

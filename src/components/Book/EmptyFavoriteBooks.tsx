d83f09230b21bb1b081c632455c6be1d33cc770f
Marcelo Oroquieta <oroquieta.m@gmail.com>
2025-02-11 20:29:43 -0300

    feat(EmptyFavoriteBooks): add image to the page

--- src/components/Book/EmptyFavoriteBooks.tsx
+++ src/components/Book/EmptyFavoriteBooks.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import Image from "next/image";
+import womenReading from "@/assets/images/women_reading.png";
 
 export function EmptyFavoriteBooks() {
   return (
     <div className="flex flex-col items-center">
+      <Image
+        src={womenReading}
+        alt="women reading a red book"
+        className="w-56 h-full object-cover"
+      />
       <span className="text-2xl">You do not have favorite books</span>
       <div className="flex flex-col items-center justify-center text-center mt-6">
         <p className="text-lg italic text-gray-600">

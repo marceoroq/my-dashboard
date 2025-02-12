2a11522504bea6aba31f1241404a1c90adee95a2
Marcelo Oroquieta <oroquieta.m@gmail.com>
2025-02-12 12:25:18 -0300

    feat(SidebarMenu): add close sidebar functionality when click in a menu item

--- src/components/Sidebar/SidebarMenuItem.tsx
+++ src/components/Sidebar/SidebarMenuItem.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import Icon from "@/components/Icon";
+import { useSidebar } from "@/context/SidebarContext";
 
 export interface SidebarMenuItemProps {
   text: string;
@@ -11,6 +13,7 @@ export interface SidebarMenuItemProps {
 
 export default function SidebarMenuItem({ url = "#", text, iconName }: SidebarMenuItemProps) {
   const currentPath = usePathname();
+  const { setIsSidebarOpen } = useSidebar();
 
   return (
     <li>
@@ -20,7 +23,8 @@ export default function SidebarMenuItem({ url = "#", text, iconName }: SidebarMe
           className="sidebar-menu-item 
                      data-[active=false]:sidebar-menu-item-common
                      data-[active=true]:sidebar-menu-item-active"
-          type="button">
+          type="button"
+          onClick={() => setIsSidebarOpen(false)}>
           <Icon name={iconName} />
           <p className="block antialiased font-sans text-base leading-relaxed text-inherit font-medium capitalize">
             {text}

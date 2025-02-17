f29fe93167b83888d9c471fa4b3ad817621c7ed8
Marcelo Oroquieta <oroquieta.m@gmail.com>
2025-02-17 10:05:47 -0300

    feat(Dashboard): hide footer

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -310,7 +310,7 @@ export default function CounterPage() {
         </div>
       </div>
       <div className="text-blue-gray-600">
-        <footer className="py-2">
+        {/* <footer className="py-2">
           <div className="flex w-full flex-wrap items-center justify-center gap-6 px-2 md:justify-between">
             <p className="block antialiased font-sans text-sm leading-normal font-normal text-inherit">
               © 2023, made with{" "}
@@ -366,7 +366,7 @@ export default function CounterPage() {
               </li>
             </ul>
           </div>
-        </footer>
+        </footer> */}
       </div>
     </>
   );
